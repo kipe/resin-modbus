722360f3908aba47542c1e241e11a6177367bc01
Kimmo Huoman <kipenroskaposti@gmail.com>
2015-11-17 16:12:25 +0200

    Re-enable dropbear...

--- app/start.sh
+++ app/start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# Set the root password as resin device UUID if not set as an ENV variable
+export PASSWD=${PASSWD:=$RESIN_DEVICE_UUID}
+echo "root:$PASSWD" | chpasswd
+dropbear -E
+
 /usr/bin/env python /app/app.py
 
 while [[ true ]]; do

75a06c245e7d6f9d09a1bbd73cf435d982fd3366
Kimmo Huoman <kipenroskaposti@gmail.com>
2015-12-16 14:14:53 +0200

    Add supervisor.

--- debian-wheezy-install.sh
+++ debian-wheezy-install.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
-apt-get update && apt-get install -y --force-yes python-pip python-dev git-core
+apt-get update && apt-get install -y --force-yes python-pip python-dev git-core supervisor
 pip install -U pip
-mkdir /data
+mkdir -p /data/modbus
 git clone https://github.com/kipe/resin-modbus.git ~/resin-modbus
 ln -s ~/resin-modbus/app /app
+cp ~/resin-modbus/configuration_example.json /data/modbus/configuration.json
 /usr/local/bin/pip install -r /app/requirements.txt
+ln -s /app/supervisor.conf /etc/supervisor/conf.d/modbus.conf
+service supervisor restart
